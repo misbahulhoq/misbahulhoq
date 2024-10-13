f454ce1b315310135c347671d4c7db38fc853e01
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2024-10-13 14:25:27 +0600

    created footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="px-4 py-9 bg-base-200 text-base-content mt-10">
+      <div className="text-center">
+        <p className="text-center">
+          © {new Date().getFullYear()} Created by Md Mezbah Uddin. All rights
+          reserved.
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
