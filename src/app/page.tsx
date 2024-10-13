9fe25128703bd9f85329ac8cef93b3b62b292b28
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2024-10-13 14:20:46 +0600

    created hire me section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import AboutMe from "@/components/AboutMe";
 import Hero from "@/components/Hero";
+import HireMe from "@/components/HireMe";
 import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
-    <main className="text-base-content space-y-10">
+    <main className="text-base-content space-y-10 lg:space-y-24">
       <Hero />
       <AboutMe />
       <Skills />
+      <HireMe />
     </main>
   );
 }
---
+++ src/components/HireMe.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const HireMe = () => {
+  return (
+    <div className="container-center">
+      <h2 className="text-5xl text-center font-bold">
+        Why Should You <span className="text-accent">Hire</span> Me?
+      </h2>
+      <div className="max-w-[900px] mx-auto space-y-4 mt-7">
+        <p>
+          You should hire me because I bring a unique blend of technical
+          expertise and creativity to every project. As a front-end developer, I
+          have a strong foundation in building scalable, user-friendly websites
+          and applications. My experience with modern frameworks like React,
+          Tailwind CSS, and tools like Firebase allows me to develop
+          high-performance interfaces quickly and efficiently. I&apos;m always
+          focused on the user experience, ensuring that my designs are both
+          functional and visually appealing.
+        </p>
+        <p>
+          In addition to my technical skills, I am constantly learning and
+          improving. I stay up to date with the latest trends in web development
+          and have a growing knowledge of data structures and algorithms, which
+          enables me to write more efficient and maintainable code. My attention
+          to detail, commitment to quality, and ability to solve complex
+          problems make me a valuable asset to any team.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HireMe;
