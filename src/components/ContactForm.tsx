5c98aef3e37ddea7cf0d9e76b9d4aa20916920fd
Md Mezbah Uddin <extraordinarymisbah@gmail.com>
2024-11-08 20:38:55 +0600

    created modal on successfull message sending

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { baseUrl } from "@/api/api";
-import React, { FormEvent } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 
 type FormData = {
   name: string;
@@ -10,8 +10,19 @@ type FormData = {
 };
 
 const ContactForm = () => {
+  const [isLoading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [modalText, setModaltext] = useState("");
+  useEffect(() => {
+    if (showModal) {
+      document.querySelector("#my_modal_5")?.classList.add("modal-open");
+    }
+  }, [showModal]);
+
   const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setLoading(true);
+    setShowModal(false);
     const formData: FormData = {
       name: (e.currentTarget.elements.namedItem("name") as HTMLInputElement)
         .value,
@@ -24,7 +35,6 @@ const ContactForm = () => {
         e.currentTarget.elements.namedItem("message") as HTMLInputElement
       ).value,
     };
-    console.log(formData);
 
     try {
       const response = await fetch(`${baseUrl}/api/email`, {
@@ -36,13 +46,18 @@ const ContactForm = () => {
       });
 
       if (!response.ok) {
+        setModaltext("Something went wrong");
         throw new Error("Failed to send message");
       }
 
-      const result = await response.json();
-      console.log("Message sent successfully:", result);
+      // const result = await response.json();
+      setModaltext("Your email has been sent successfully!");
+      setShowModal(true);
+      setLoading(false);
     } catch (error) {
-      console.log("Error:", error);
+      setLoading(false);
+      console.error("Error:", error);
+      setModaltext("Something went wrong");
     }
   };
   return (
@@ -60,6 +75,7 @@ const ContactForm = () => {
             id="name"
             placeholder="Enter your full name ..."
             className="input input-bordered w-full"
+            required
           />
         </div>
 
@@ -72,6 +88,7 @@ const ContactForm = () => {
             id="email"
             placeholder="Enter your email ..."
             className="input input-bordered w-full"
+            required
           />
         </div>
 
@@ -84,6 +101,7 @@ const ContactForm = () => {
             id="subject"
             placeholder="Enter the subject ..."
             className="input input-bordered w-full"
+            required
           />
         </div>
 
@@ -96,13 +114,67 @@ const ContactForm = () => {
             placeholder="Enter your message ..."
             rows={10}
             className="input input-bordered min-h-44 w-full py-3"
+            required
           ></textarea>
         </div>
 
-        <button className="btn btn-primary max-[400px]:btn-block">
-          Submit
-        </button>
+        {isLoading ? (
+          <button className="btn btn-disabled max-[400px]:btn-block">
+            <span className="loading loading-spinner"></span>
+            Sending...
+          </button>
+        ) : (
+          <button
+            disabled={isLoading}
+            className="btn btn-primary max-[400px]:btn-block"
+          >
+            Submit
+          </button>
+        )}
       </form>
+
+      {showModal && (
+        <>
+          <dialog
+            id="my_modal_5"
+            className="modal modal-open modal-bottom sm:modal-middle"
+          >
+            <div className="modal-box">
+              <div role="alert" className="alert alert-success py-7">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 shrink-0 stroke-current"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <h3 className="text-lg font-semibold">{modalText}</h3>
+              </div>
+              <div className="modal-action">
+                <form method="dialog">
+                  {/* if there is a button in form, it will close the modal */}
+                  <button
+                    onClick={() => {
+                      document
+                        .querySelector("#my_modal_5")
+                        ?.classList.remove("modal-open");
+                    }}
+                    className="btn btn-warning"
+                  >
+                    Close
+                  </button>
+                </form>
+              </div>
+            </div>
+          </dialog>
+        </>
+      )}
     </div>
   );
 };
