a97305e36ed5af8900de671d4602601c19c392d2
Md Mezbah Uddin <extraordinarymisbah@gmail.com>
2024-12-09 21:40:47 +0600

    updated metadata for seo ranking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,45 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+// export const metadata: Metadata = {
+//   title: "Md Mezbah Uddin",
+//   description: "Portfolio of Md Mezbah Uddin",
+// };
 export const metadata: Metadata = {
-  title: "Md Mezbah Uddin",
-  description: "Portfolio of Md Mezbah Uddin",
+  title: {
+    template: "%s | Md Mezbah Uddin",
+    default: "Md Mezbah Uddin",
+  },
+  description:
+    "Welcome to the portfolio of Md Mezbah Uddin, a skilled front-end developer specializing in React, Next.js, and Tailwind CSS. Explore my projects, skills, and contact details.",
+  keywords: [
+    "Md Mezbah Uddin",
+    "Mezbah",
+    "front-end developer",
+    "React developer",
+    "Next.js portfolio",
+    "Tailwind CSS projects",
+    "web developer portfolio",
+    "JavaScript developer",
+  ],
+  authors: [{ name: "Md Mezbah Uddin", url: "https://misbahulhoq.vercel.app" }],
+  robots: "index, follow",
+  openGraph: {
+    title: "Md Mezbah Uddin | Front-End Developer Portfolio",
+    description:
+      "Explore the portfolio of Md Mezbah Uddin, showcasing expertise in front-end development, including React, Next.js, and Tailwind CSS.",
+    url: "https://misbahulhoq.vercel.app", // Replace with your actual domain
+    siteName: "Md Mezbah Uddin Portfolio",
+    images: [
+      {
+        url: "/mezbah-photo.png", // Add a relevant image for preview
+        width: 1200,
+        height: 630,
+        alt: "Portfolio Preview of Md Mezbah Uddin",
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({
