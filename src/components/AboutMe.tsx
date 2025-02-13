600d7bcbb943263d74cd17e19a4d0dce86f79742
Misbah Ul Hoq <extraordinarymisbah@gmail.com>
2025-02-13 19:38:02 +0600

    a space

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -14,7 +14,7 @@ const AboutMe = () => {
           <div className="left-content image-wrapper hidden lg:block">
             <Image
               src="/ilustrations/using-laptop-two.webp"
-              alt="Computer Illustration"
+              alt="Computer  Illustration"
               height={500}
               width={500}
               className="h-[350px] w-[350px] rounded-full object-cover"
