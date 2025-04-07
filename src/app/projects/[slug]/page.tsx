1caf42025a6355a1f87f15f86a3aad70284768ec
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-04-07 19:33:03 +0600

    updated the project details

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,10 +1,15 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
+import { readFileSync } from "fs";
+import "../../../styles/editor.css";
 export default function Page({ params }: { params: { slug: string } }) {
-  console.log(params.slug);
+  const workDir =
+    process.cwd() + "/src/contents/projects/" + params.slug + ".mdx";
+
+  const content = readFileSync(workDir, "utf8");
+
   return (
-    <div className="container-center">
-      {" "}
-      <MDXRemote source={`## ${params.slug}`} />
+    <div className="container-center project-details py-6">
+      <MDXRemote source={content} />
     </div>
   );
 }
